bad695fdb998ebbd5275f16210e5c919544df4b0
Kajaaa9 <kb06956@student.uni-lj.si>
2025-07-17 19:27:59 +0200

    preimenovanje funkcij, datotek (manjsi popravki)

--- oi1_main.py
+++ oi1_main.py
@@ -3,8 +3,8 @@ import oi3_izlusci
 import oi4_shrani
 import os
 
-if not os.path.exists("oi5_spletnastran.html"):     # shrani spletno stran kot html
-    oi2_pridobi.shrani_url__kot_html()
+if not os.path.exists("oi5_olimpijske-igre.html"):     # shrani spletno stran kot html
+    oi2_pridobi.shrani_url_kot_html()
 
-if not os.path.exists("oi6_linki_po-igrah.csv"):    # shrani htmlje po igrah (letih) v csv
-    oi2_pridobi.shrani_htmlje_po_igrah2()
+if not os.path.exists("oi6_url_po-igrah.csv"):    # shrani htmlje po igrah (letih) v csv
+    oi2_pridobi.shrani_url_po_igrah()
--- oi2_pridobi.py
+++ oi2_pridobi.py
@@ -7,21 +7,21 @@ url = "https://www.olympics.com/en/olympic-games"
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
 
 
-def shrani_url__kot_html():
-    with open("oi5_spletnastran.html", "w", encoding="utf-8") as f:
+def shrani_url_kot_html():
+    with open("oi5_olimpijske-igre.html", "w", encoding="utf-8") as f:
         f.write(requests.get(url, headers=headers).text)
 
 
-def shrani_htmlje_po_igrah2():
+def shrani_url_po_igrah():
 
-    with open("oi5_spletnastran.html", "r", encoding="utf-8") as f:
+    with open("oi5_olimpijske-igre.html", "r", encoding="utf-8") as f:
         url_html = f.read()
 
     vzorec = r'"@type":"ListItem",\s*"position":(\d+),\s*"url":"(https://www\.olympics\.com/olympic-games/([^"]+))"'
 
     matches = re.findall(vzorec, url_html, re.DOTALL)
     
-    with open("oi6_linki_po-igrah.csv", "w", newline="", encoding="utf-8") as csvfile:
+    with open("oi6_url_po-igrah.csv", "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["Zaporedna številka", "URL", "Mesto-Leto"])  # Trije stolpci
         for poz, url, mestoleto in matches:
