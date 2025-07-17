1799fb8ead419aaeaa6de5d36be5bad8ea039e39
Kajaaa9 <kb06956@student.uni-lj.si>
2025-07-17 19:17:10 +0200

    napisala funkcije za pridobi (spletno stran shrani kot html file, pobere ven iskane linke in jih da v csv file)

---
+++ oi1_main.py
@@ -0,0 +1,13 @@
+import oi2_pridobi
+import oi3_izlusci
+import oi4_shrani
+import os
+
+if not os.path.exists("oi5_spletnastran.html"):     # shrani spletno stran kot html
+    oi2_pridobi.shrani_url__kot_html()
+
+if not os.path.exists("oi6_linki_po-igrah.csv"):    # shrani htmlje po igrah (letih) v csv
+    oi2_pridobi.shrani_htmlje_po_igrah()
+
+if not os.path.exists("oi7_linki_po-igrah-mestih.csv"):    # shrani htmlje po igrah v csv, dodaten stolpec za mesto-leto
+    oi2_pridobi.shrani_htmlje_po_igrah2()
---
+++ oi2_pridobi.py
@@ -0,0 +1,45 @@
+import requests
+import csv
+import re
+
+
+url = "https://www.olympics.com/en/olympic-games"
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+
+
+def shrani_url__kot_html():
+    with open("oi5_spletnastran.html", "w", encoding="utf-8") as f:
+        f.write(requests.get(url, headers=headers).text)
+
+
+def shrani_htmlje_po_igrah():
+
+    with open("oi5_spletnastran.html", "r", encoding="utf-8") as f:
+        url_html = f.read()
+
+    vzorec = r'"@type":"ListItem",\s*"position":(\d+),\s*"url":"(https://[^"]+)"'   # blok znotraj katerega iscem pozicije, linke po igrah
+    # vzorec = r'"@type":"ListItem",\s*"position":\d+,\s*"url":"(https://[^"]+)"'   # blok znotraj katerega iscem linke po igrah
+
+    matches = re.findall(vzorec, url_html, re.DOTALL)
+    
+    with open("oi6_linki_po-igrah.csv", "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["URL"])
+        for poz, url in matches:
+            writer.writerow([url])
+
+
+def shrani_htmlje_po_igrah2():
+
+    with open("oi5_spletnastran.html", "r", encoding="utf-8") as f:
+        url_html = f.read()
+
+    vzorec = r'"@type":"ListItem",\s*"position":(\d+),\s*"url":"(https://www\.olympics\.com/olympic-games/([^"]+))"'
+
+    matches = re.findall(vzorec, url_html, re.DOTALL)
+    
+    with open("oi7_linki_po-igrah-mestih.csv", "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Zaporedna številka", "URL", "Mesto-Leto"])  # Trije stolpci
+        for poz, url, mestoleto in matches:
+            writer.writerow([poz, url, mestoleto])
